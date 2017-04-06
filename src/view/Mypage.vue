c81d1981b5630421ccb4af01ffc17dfd101f85e8
WenQi XIA <wenqi.xia@fengchaohuzhu.com>
2017-04-06 22:34:08 +0800

    create the top scroll bar

--- src/view/Mypage.vue
+++ src/view/Mypage.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="mypage">
         <p class="content">我的</p>
+        <a href="#" class="weui-btn weui-btn_primary">按钮</a>
+        <a href="#" class="weui-btn weui-btn_disabled weui-btn_primary">按钮</a>
+        <a href="#" class="weui-btn weui-btn_warn">确认</a>
+        <a href="#" class="weui-btn weui-btn_disabled weui-btn_warn">确认</a>
+        <a href="#" class="weui-btn weui-btn_default">按钮</a>
+        <a href="#" class="weui-btn weui-btn_disabled weui-btn_default">按钮</a>
+        <div class="button-sp-area">
+            <a href="#" class="weui-btn weui-btn-plain-default">按钮</a>
+            <a href="#" class="weui-btn weui-btn-plain-primary">按钮</a>
+
+            <a href="#" class="weui-btn weui-btn-mini weui-btn-primary">按钮</a>
+            <a href="#" class="weui-btn weui-btn-mini weui-btn-default">按钮</a>
+        </div>
   </div>
 </template>
 
@@ -15,6 +28,7 @@
     }
 </script>
 
+<style  src="../../static/css/weui.css"></style>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
     .content{
